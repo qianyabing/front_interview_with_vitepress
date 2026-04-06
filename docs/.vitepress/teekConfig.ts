a22c50df21486ec43ba80a5194b9ff3d5e12d1d0
qianyabing <2534150597@qq.com>
2026-04-06 22:11:25 +0800

    页面宽度调整，优化PDF切换

--- docs/.vitepress/teekConfig.ts
+++ docs/.vitepress/teekConfig.ts
@@ -6,6 +6,37 @@ export const teekConfig = defineTeekConfig({
   vpHome: true, // 是否隐藏 VP 首页
   sidebarTrigger: true, // 是否开启侧边栏折叠功能
   author: { name: "Teeker", link: "https://github.com/Kele-Bingtang" },
+  themeSize: "large",
+  themeEnhance: {
+    enabled: true, // 启用主题增强功能
+    position: "top", // 位置，top 为导航栏右侧，bottom 为右下角
+    // 布局切换配置
+    layoutSwitch: {
+      disabled: false, // 禁用布局切换
+      defaultMode: "bothWidthAdjustable", // 布局切换的默认模式
+      disableHelp: false, // 禁用帮助提示
+      disableAnimation: false, // 禁用布局切换动画
+      defaultDocMaxWidth: 90, // 内容布局最大宽度的默认百分比，仅限 0-100
+      disableDocMaxWidthHelp: false, // 禁用帮助提示
+      defaultPageMaxWidth: 95, // 页面布局最大宽度的默认百分比，仅限 0-100
+      disablePageMaxWidthHelp: false, // 禁用帮助提示
+    },
+    // 布局主题色配置
+    themeColor: {
+      disabled: false, // 禁用布局主题色切换
+      defaultColorName: "vp-default", // 布局默认主题色
+      defaultSpread: false, // 是否将主题色扩散到其他元素（根据主题色计算其他元素需要的颜色）
+      disableHelp: false, // 禁用帮助提示
+      disabledInMobile: false, // 是否在移动端禁用
+    },
+    // 聚光灯配置
+    spotlight: {
+      disabled: false, // 禁用聚光灯
+      defaultStyle: "aside", //  聚光灯默认样式
+      disableHelp: false, // 禁用帮助提示
+      defaultValue: true, // 聚光灯默认开关状态
+    },
+  },
   footerInfo: {
     theme: {
       name: `Theme By Teek@${version}`,
